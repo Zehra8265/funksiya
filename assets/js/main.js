e3a95c33c121fb613f19843aaba3be238df0b437
Zehra <127186223+Zehra8265@users.noreply.github.com>
2023-04-27 08:01:06 +0300

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,27 @@
+
+
+
+// var text=prompt("Metn daxil edin");
+// var arr=prompt("deyer daxil edin");
+// var count=0;
+// for(let i=0; i<text.length;i++){
+//     if(arr==text[i]){
+//         count++;
+//     }
+// }
+// alert(count);
+
+
+
+var text=("Zehraa");
+var count=0;
+letresult=text.includes("a")
+for (var i=0 ;i<text.length;i++){
+    if(text[i]==="a"){
+    count=count+1
+}
+}
+alert(count);
+
+
+
